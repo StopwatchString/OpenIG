bf85b484a4255d440c3831d1d0a04860c412db1e
Mason Speck <mcs.5299@gmail.com>
2024-07-09 20:25:13 -0400

    Vulkan init

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-const std::string WINDOW_NAME = "OpenIG";
+const char* APPLICATION_NAME = "OpenIG";
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
@@ -16,11 +16,41 @@ int main() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Don't make an OpenGL context
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);   // TODO:: Implement window resizing
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, WINDOW_NAME.c_str(), nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, APPLICATION_NAME, nullptr, nullptr);
 
     //--------------
     // Init Vulkan
     //--------------
+
+    // Create VKInstance
+    VkInstance instance;
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = APPLICATION_NAME;
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if (result != VK_SUCCESS) {
+        std::cout << "Failed to create VkInstance." << std::endl;
+        return EXIT_FAILURE;
+    }
 
     //--------------
     // Main Loop
@@ -32,9 +62,11 @@ int main() {
     //--------------
     // Cleanup
     //--------------
+
+    vkDestroyInstance(instance, nullptr);
+
     glfwDestroyWindow(window);
     glfwTerminate();
-
 
     return EXIT_SUCCESS;
 }
